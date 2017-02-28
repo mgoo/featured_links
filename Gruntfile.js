a5ed6fb6e254f2cccc0932b463da387c652f3805
mgoo <andrewmcghienz@gmail.com>
2017-03-01 11:35:19 +1300

    fixed the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,14 +10,35 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
 
-
+    /**
+     * Function to generate the destination for the uglify task
+     * (e.g. build/file.min.js). This function will be passed to
+     * the rename property of files array when building dynamically:
+     * http://gruntjs.com/configuring-tasks#building-the-files-object-dynamically
+     *
+     * @param {String} destPath the current destination
+     * @param {String} srcPath the  matched src path
+     * @return {String} The rewritten destination path.
+     */
+    var uglifyRename = function(destPath, srcPath) {
+        destPath = srcPath.replace('src', 'build');
+        destPath = destPath.replace('.js', '.min.js');
+        return destPath;
+    };
 
     grunt.initConfig({
         watch: {
-            // If any .less file changes in directory "less" then run the "less" task.
-            files: "less/*.less",
-            tasks: ["less"]
+            less: {
+                // If any .less file changes in directory "less" then run the "less" task.
+                files: "less/*.less",
+                tasks: ["less"]
+            },
+            uglify: {
+                files: 'amd/src/*.js',
+                tasks: ["uglify", "jshint"]
+            }
         },
         less: {
             // Production config is also available.
@@ -31,9 +52,18 @@ module.exports = function (grunt) {
                 files: {
                     "styles.css": "less/styles.less"
                 }
-            },
+            }
+        },
+        uglify: {
+            amd: {
+                files: [{src: 'amd/src/*.js', expand: true, rename: uglifyRename}],
+                options: {report: 'none'}
+            }
+        },
+        jshint: {
+            files: "amd/src/*.js",
         }
     });
     // The default task (running "grunt" in console).
-    grunt.registerTask("default", ["less"]);
+    grunt.registerTask("default", ["watch"]);
 };
